d98abfa1a332786ae7fac5e39a7a4ccda06ad09b
Shaotong Sun <55509173+sunshaot@users.noreply.github.com>
2022-05-09 22:01:55 -0400

    Update main.py

--- Building_Pong/main.py
+++ Building_Pong/main.py
@@ -20,3 +20,13 @@ def go_up():
 def go_down():
     new_y = paddle.ycor() - 20
     paddle.goto(paddle.xcor(), new_y)
+
+screen.listen()
+screen.onkey(go_up, "Up")
+screen.onkey(go_down, "Down")
+
+game_on = True
+while game_on:
+    screen.update()
+
+screen.exitonclick()
